898c661c3e995caac3ea40b9c4dbea230a42ff50
shtabnov <90615010+shtabnov@users.noreply.github.com>
2023-03-06 16:02:07 +0500

    update styles

--- src/components/BrigadesCard.jsx
+++ src/components/BrigadesCard.jsx
@@ -1,46 +1,46 @@
-import React from "react";
-import { Card } from "antd";
-import { CheckCircleOutlined, StopOutlined } from "@ant-design/icons";
-import styles from "./BrigadesCard.module.css";
+import React from 'react';
+import { Card } from 'antd';
+import { CheckCircleOutlined, StopOutlined } from '@ant-design/icons';
+import styles from './BrigadesCard.module.css';
 
 const BrigadesCard = ({ brigade, department, connection }) => {
-    const { brigade_name, position } = brigade;
+  const { brigade_name, position } = brigade;
 
-    return (
-        <div className={styles.brigada}>
-            <Card size="small" title={brigade_name} bordered={true}>
-                <div className={styles.description}>
-                    <h3>{department[0].name}</h3>
-                    <span
-                        className={
-                            connection[0].connectionStateId
-                                ? styles.connection
-                                : styles.connection_without
-                        }
-                    >
-                        <b>Соединение: </b> {connection[0].name + " "}
-                        <span>
-                            {connection[0].connectionStateId ? (
-                                <CheckCircleOutlined />
-                            ) : (
-                                <StopOutlined />
-                            )}
-                        </span>
-                    </span>
+  return (
+    <div className={styles.brigada}>
+      <Card size="small" title={brigade_name} bordered={true}>
+        <div className={styles.description}>
+          <h3>{department[0].name}</h3>
+          <span
+            className={
+              connection[0].connectionStateId
+                ? styles.connection
+                : styles.connection_without
+            }
+          >
+            <b>Соединение: </b> {connection[0].name}
+            <span>
+              {connection[0].connectionStateId ? (
+                <CheckCircleOutlined />
+              ) : (
+                <StopOutlined />
+              )}
+            </span>
+          </span>
 
-                    <span>
-                        <b>Кластер</b>: {position.field}
-                    </span>
-                    <span>
-                        <b>Поле</b>: {position.cluster}
-                    </span>
-                    <span>
-                        <b>Скважина</b>: {position.well}
-                    </span>
-                </div>
-            </Card>
+          <span>
+            <b>Кластер</b>: {position.field}
+          </span>
+          <span>
+            <b>Поле</b>: {position.cluster}
+          </span>
+          <span>
+            <b>Скважина</b>: {position.well}
+          </span>
         </div>
-    );
+      </Card>
+    </div>
+  );
 };
 
 export default BrigadesCard;
